be0f4f1d94ef62b1380692759ef999c1d6fa0f4a
AYUSHBHAGAT59 <96487087+AYUSHBHAGAT59@users.noreply.github.com>
2022-05-02 19:10:42 +0530

    Add files via upload

---
+++ php_programme/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "this title is written with "; echo "php code"; ?></title>
+</head>
+<body>
+<?php
+echo "hello world <br> <br>";
+echo '<h1> this is my first php programme <h1> <br>';
+echo "5+5 = ",5+5;
+
+?>    
+<h1><?php echo'<a href="youtube.com" target="_blank">this is a link</a>'?></h1>
+</body>
+</html>
